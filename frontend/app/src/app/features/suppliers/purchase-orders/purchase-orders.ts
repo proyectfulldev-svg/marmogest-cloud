688d58b7d1d09734e07574518bedf286a867f8e1
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2026-02-16 15:34:03 -0500

    feat: create project structure with core, features and layout modules

---
+++ frontend/app/src/app/features/suppliers/purchase-orders/purchase-orders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-orders',
+  imports: [],
+  templateUrl: './purchase-orders.html',
+  styleUrl: './purchase-orders.css',
+})
+export class PurchaseOrders {
+
+}
